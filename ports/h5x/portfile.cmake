600d7725eb632d5b03f277a0e1b3ec5e359c0735
serge klim <serge.klim@outlook.com>
2024-12-28 20:41:30 +0000

    h5x

---
+++ ports/h5x/portfile.cmake
@@ -0,0 +1,35 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO serge-klim/h5x
+    REF "v${VERSION}"
+    SHA512 2ef1b2f22eee09e546955d606cd5dcda60032ecfd10818b4f504305c1c30f4ec69fe867b360891fb1e8497fef7cefc4172b0fe9e94b64756020d8d61d390d830
+    HEAD_REF main
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+   FEATURES
+     "tools"      BUILD_TOOLS
+     "tests"      BUILD_TESTS
+)
+
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    set(HDF5_LINK_STATICALLY ON)
+else()
+    set(HDF5_LINK_STATICALLY OFF)
+endif()
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" HDF5_LINK_STATICALLY)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+       -DCMAKE_CXX_STANDARD=20
+       -DVCPKG_TARGET_TRIPLET=ON
+       ${FEATURE_OPTIONS}
+       -DHDF5_LINK_STATICALLY=${HDF5_LINK_STATICALLY}
+       -DBUILD_BENCHMARK=OFF       
+)
+
+vcpkg_cmake_install()
+
